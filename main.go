c20afe757d00806d14275be79c09d3395bd16109
mvstermind <zablockidawid1@gmail.com>
2024-06-30 14:06:06 +0200

    go reads properly json output from python

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// SpeedData containts string type beacuse python part returns formatted string with Mbps ms etc.
+type SpeedData struct {
+	DownloadSpeed string `json:"download_speed"`
+	UploadSpeed   string `json:"upload_speed"`
+	Ping          string `json:"ping"`
+}
+
+func main() {
+	cmd := exec.Command("python3", "speed.py")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+
+	fmt.Println(string(output))
+}
